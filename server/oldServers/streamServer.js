ea25bc823402021a48fc2b66c86d3849e1b16cc5
kylemondina <kylemondinaportfolio@gmail.com>
2022-10-31 00:38:51 -0700

    added a container log api

--- server/oldServers/streamServer.js
+++ server/oldServers/streamServer.js
@@ -11,29 +11,44 @@ const HOST = '0.0.0.0';
 const app = express();
 
 const containerLogs = (container,res)=>{
+	//let logStream = new stream.PassThrough();
+//	logStream.on('data', (chunk)=>{
+		//console.log(chunk.toString('utf8'));
+		//logStream.pipe(res);
+		//res.send(chunk.toString('utf8'));
+	//})
 	let chunks = "";
-
-	let logStream = new stream.PassThrough();
-	logStream.on('data', (chunk)=>{
-		console.log(chunk.toString('utf8'));
-		chunks = chunks.concat(chunk.toString('utf8'));
-	})
 	container.logs({
 		follow:true,
 		stdout:true,
 		stderr:true
 	},(err,stream)=>{
 		if (err) {console.log(err)}
-		container.modem.demuxStream(stream,logStream,logStream);
+		//container.modem.demuxStream(stream,logStream,logStream);
+		stream.on('data',(chunk)=>{
+			//stream.pipe(res);
+			/*console.log("-----new chunk----")
+			console.log(chunk.toString('utf8'))
+			console.log("-----end of new chunk----")
+			res.write("-----new chunk----\n")
+			res.write(chunk.toString('utf8')+'\n','utf8');
+			res.write("-----end of new chunk----\n")
+			*/
+			console.log(chunk.toString('utf8'))
+			chunks = chunks.concat(chunk.toString('utf8'));
+			//chunks.push(chunk.toString('utf8'));
+		})
 		stream.on('end', ()=>{
-			logStream.end('!stop!');
+			//logStream.end('!stop!');
 			res.writeHead(200,{
 				'Content-Length':Buffer.byteLength(chunks),
 			});
 			res.write(chunks);
 			res.end('!stop!');
-
 		})
+		/*setTimeout(()=>{
+			//stream.destroy();
+		},2000)*/
 	})
 }
 
@@ -49,8 +64,8 @@ app.get("/containers", (req,res)=>{
 
 app.get("/containers/:id",(req,res)=>{
 	res.setHeader('Content-Type', 'text/html');
+	//res.setHeader('Transfer-Encoding', 'chunked');
 	let rqstedContainer = docker.getContainer(req.params.id)
-	//we need to add an error here
 	containerLogs(rqstedContainer,res);
 })
 
